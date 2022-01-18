86f41c3bf4f3c8f346fceb54169c91bf9dc67106
edualves12 <40210473@esmad.ipp.pt>
2022-01-18 00:44:59 +0000

    musica de fundo

---
+++ music_script.js
@@ -0,0 +1,20 @@
+const button = document.querySelector("button")
+var cont = 0
+
+function play(){
+    if(cont == 0) {
+        cont = 1
+        const audio = document.querySelector("audio")
+        audio.currentTime = 0.1
+        audio.volume = 0.05
+        audio.play()
+        document.getElementById("btn_play").src="galery/dino.gif" 
+    } else {
+        cont = 0
+        const audio = document.querySelector("audio")
+        audio.currentTime = 0
+        audio.pause()
+        document.getElementById("btn_play").src="galery/play_btn.png" 
+    }
+}
+
