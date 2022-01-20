0a987a42032e5753db314a971b16d234917ed9ce
edualves12 <40210473@esmad.ipp.pt>
2022-01-20 23:53:00 +0000

    final countdown

--- script.js
+++ script.js
@@ -15,11 +15,25 @@ function create_button() {
             let link_checkout = document.createElement("a");
             let img_checkout = document.createElement("img");
             let p_checkout = document.createElement("p");
+            let btn_delete = document.createElement("button");
+            let img_btn_delete = document.createElement("img");
+            let div_drop = document.createElement("div")
+            img_btn_delete.className = "img_delete"
+            img_btn_delete.src = "/galery/x.png"
+            btn_delete.append(img_btn_delete)
+            btn_delete.className = "delete"
+            btn_delete.addEventListener("click", function() {
+                this.parentNode.remove();
+            });
             p_checkout.append(div_product.children[0].children[1].children[0].innerText + " " + div_product.children[0].children[1].children[1].innerText);
             img_checkout.src = div_product.children[0].children[0].children[0].src;
             link_checkout.append(img_checkout);
             link_checkout.append(p_checkout);
-            drop_menu.append(link_checkout);
+            link_checkout.href = div_product.children[0].href
+            div_drop.append(link_checkout);
+            div_drop.append(btn_delete)
+            div_drop.className = "div_drop"
+            drop_menu.append(div_drop);
         });
         div_product.appendChild(btn);
     }
